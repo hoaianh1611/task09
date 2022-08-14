6f2ed37c2f81d42c940517c6b6e333dc0dba38aa
Sophie Nguyen <anh.nguyen.pd@gmail.com>
2022-08-15 00:06:48 +0700

    update

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -1,47 +1,45 @@
 /* eslint-disable linebreak-style */
 
-// $(function () {
-//     slideinout();
-// });
+$(function () {
+    slideinout();
+});
+  
+function slideinout() {
+    if (!$("#c-jsmainslider").length) return;
+    var slider = $("#c-jsmainslider");
+    slider.on("init", function () {
+        slider.find(".slick-current").removeClass("slick-animated");
+    });
 
-// function slideinout() {
-//     if (!$("#c-slider").length) return;
-//     var slider = $("#c-slider");
-//     slider.on("init", function () {
-//         slider.find(".slick-current").removeClass("slick-animated");
-//     });
-
-//     slider.slick({
-//         fade: true,
-//         dots: false,
-//         arrows: false,
-//         autoplay: false,
-//         autoplaySpeed: 5000,
-//         speed: 2000,
-//         cssEase: "cubic-bezier(0.25, 0.1, 0.25, 1)",
-//         pauseOnFocus: false,
-//         pauseOnHover: false,
-//         swipe: false,
-//     });
-//     slider.on("beforeChange", function (event, slick, currentSlide, nextSlide) {
-//         slick.$slides.eq(nextSlide).addClass("slick-animated");
-//         setTimeout(function () {
-//             slider
-//                 .find(".slick-slide:not(.slick-current)")
-//                 .removeClass("slick-animated");
-//         }, 1500);
-//     });
-//     setTimeout(function () {
-//         slider.slick("slickPlay");
-//         slider.find(".slick-current").addClass("slick-animated");
-//     }, 2000);
-// }
+    slider.slick({
+        fade: true,
+        dots: false,
+        arrows: false,
+        autoplay: false,
+        autoplaySpeed: 5000,
+        speed: 2000,
+        cssEase: "cubic-bezier(0.25, 0.1, 0.25, 1)",
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        swipe: false,
+    });
+    slider.on("beforeChange", function (event, slick, currentSlide, nextSlide) {
+        slick.$slides.eq(nextSlide).addClass("slick-animated");
+        setTimeout(function () {
+            slider
+                .find(".slick-slide:not(.slick-current)")
+                .removeClass("slick-animated");
+        }, 1500);
+    });
+    setTimeout(function () {
+        slider.slick("slickPlay");
+        slider.find(".slick-current").addClass("slick-animated");
+    }, 2000);
+}
 
 // Modal
 $( document ).ready(function() {
     $(".c-modalopen").click(function(){
-        console.log($(this).parent().find(".c-modal"));
-        console.log("click");
         $(this).parent().find(".c-modal").show();
         $("body").css("overflow", "hidden");
     });
@@ -51,11 +49,15 @@ $( document ).ready(function() {
         $("body").css("overflow", "visible");
     });
 
-    $(".c-modal").click(function(event){
-        event.stopPropagation();
+    $(".c-modal").click(function(){
         $(".c-modal").hide();
         $("body").css("overflow", "visible");
     });
+
+    $(".c-modalwrap").click(function(event){
+        event.stopPropagation();
+    });
+    
 });
 
 
@@ -75,19 +77,59 @@ window.onscroll = () =>{
                 if(document.querySelector("header nav a[href*=" + id + "]")){
                     document.querySelector("header nav a[href*=" + id + "]").classList.add("is-active");
                 }
-                
             });
         }
     });
 };
 
-// $(".u-anchor").click(function(){
-//     $(".c-active").removeClass("c-active");
-//     $(this).addClass("c-active");
-// });
+// Open Tab
+function openTab(e, tabNo) {
+    // Declare all variables
+    var i, tabcontent, tabbtn;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("c-recruit__tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tabbtn = document.getElementsByClassName("c-recruit__tabcontrolbtn");
+    for (i = 0; i < tabbtn.length; i++) {
+        tabbtn[i].className = tabbtn[i].className.replace(" c-tabactive", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabNo).style.display = "block";
+    e.currentTarget.className += " c-tabactive";
+}
 
-// Tab
+
+//function to open menu mobile
+$(document).ready(function(){
+    $(".c-breadcrumb__menuicon").click(function(){
+        $(this).parent().find(".c-gnav").show();
+        $(this).children().find(".c-menuopen").hide();
+        $(this).children().find(".c-menuclose").show();
+    });
+});
 
 
+//function to open menu mobile
+$(document).ready(function(){
+    $(".c-breadcrumb__menuicon").click(function(){
+        console.log($(this).parent());
+        console.log($(this).children());
+        $(this).parent().find(".c-gnav").toggleClass("is-show");
+        $(this).children().find(".c-menuopen").toggleClass("is-hide");
+        $(this).children().find(".c-menuclose").toggleClass("is-show");
+        $(".body").toggleClass("u-overflow");
+        
+    });
 
-
+    $(".c-gnavmobile").find("li").click(function(){
+        $(".c-gnavmobile").toggleClass("u-display--block");
+        $(".c-gnavicon__open").toggleClass("u-display--block");
+        $(".c-gnavicon__close").toggleClass("u-display--none");
+    });
+});
